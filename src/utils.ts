3040f69548c0942524d69a813e69b8703f41d782
John Doe <johndoe@example.com>
2025-06-08 23:47:54 +0200

    feat: add utility functions for managing front matter and file operations in ThoughtStream

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,101 @@
+import { type App, Notice, Plugin, TFile } from "obsidian";
+
+export const updateFrontMatterByFile = async (
+	app: App,
+	file: TFile,
+	finalData: any
+) => {
+	await app.fileManager.processFrontMatter(file, (fm) => {
+		Object.keys(finalData).forEach((key) => {
+			fm[key] = finalData[key];
+		});
+	});
+};
+
+export const getFrontMatterByFile = async (app: App, file: TFile) => {
+	let frontmatter = {} as Record<string, any>;
+	await app.fileManager.processFrontMatter(file, (fm) => {
+		frontmatter = fm;
+	});
+	return frontmatter;
+};
+
+export const handleLocalUrl = async (obUrl: string, plugin: Plugin) => {
+	console.log("handleLocalUrl--", obUrl);
+
+	try {
+		const obInnerFile = plugin.app.metadataCache.getFirstLinkpathDest(obUrl, "");
+		if (!obInnerFile) {
+			new Notice(`Failed upload ${obUrl}, 文件不存在`);
+			return;
+		}
+		console.log("find ob local file");
+
+		const conArrayBuffer = await plugin.app.vault.readBinary(obInnerFile);
+
+		return new Blob([conArrayBuffer], {
+			type: "image/" + obInnerFile.extension,
+		});
+	} catch (error: any) {
+		new Notice(`Fail Read ${obUrl}, ${error.message}`);
+	}
+
+	return;
+};
+
+export function hasFrontmatterKey(app: App, file: TFile, frontmatterKey: string) {
+	if (!file) return false;
+	const cache = app.metadataCache.getFileCache(file);
+	return !!cache?.frontmatter?.[frontmatterKey];
+}
+
+export function getDataViewCache(app: App, linkedFile: TFile, sourceFile: TFile) {
+	if (hasDataviewActive(app)) {
+		return getDataviewApi(app).page(linkedFile.path, sourceFile.path);
+	}
+}
+
+export function getActiveFile(app: App): TFile|null {
+	return app.workspace.getActiveFile();
+}
+
+export function refreshActiveFile(app: App): void {
+	app.workspace.activeEditor?.editor?.refresh();
+}
+
+export async function getCachedFileContent(app: App, file: TFile): Promise<string> {
+	return app.vault.cachedRead(file);
+}
+
+export async function getFileContent(app: App, file: TFile): Promise<string> {
+	return app.vault.read(file);
+}
+
+export function cleanFileName(fileName: string): string {
+	return fileName.replace(/[^a-zA-Z0-9-_ ]/g, '').trim();
+}
+
+export function hasDataviewActive(app: App): boolean {
+	return (app as any).plugins.enabledPlugins.has('dataview') &&
+		(app as any).plugins?.plugins?.dataview?.api;
+}
+
+export function getDataviewApi(app: App): any {
+	return (app as any).plugins?.plugins?.dataview?.api;
+}
+
+export const getAllThoughtStreamFiles = async (app: App, identifier: string = '#thoughtstream'): Promise<TFile[]> => {
+	if (hasDataviewActive(app)) {
+		return await getDataviewApi(app).pages(identifier) as TFile[] ?? [];
+	}
+	new Notice('Dataview not active.')
+	return [];
+}
+
 export function getBaseFileName(filePath: string) {
 	// Extract the file name including extension
 	const fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
 
 	// Remove the extension from the file name
-	const baseFileName = fileName.substring(0, fileName.lastIndexOf("."));
-
-	return baseFileName;
+	return fileName.substring(0, fileName.lastIndexOf("."));
 }
