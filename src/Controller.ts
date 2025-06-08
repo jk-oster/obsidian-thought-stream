bde9031d1d39d1eece170c2890e8c1bc356bfbb6
John Doe <johndoe@example.com>
2025-06-08 23:47:04 +0200

    [FEATURE] add Controller class for managing recording operations in ThoughtStream

---
+++ src/Controller.ts
@@ -0,0 +1,35 @@
+import {RecordingStatus} from "./StatusBar";
+import ThoughtStream from "../main";
+
+export class Controller {
+	private readonly plugin: ThoughtStream;
+
+	constructor(plugin: ThoughtStream) {
+		this.plugin = plugin;
+	}
+
+	public async startRecording() {
+		console.log("start");
+		this.plugin.statusBar.updateStatus(RecordingStatus.Recording);
+		await this.plugin.recorder.startRecording();
+		this.plugin.timer.start();
+	}
+
+	public async pauseRecording() {
+		console.log("pausing recording...");
+		await this.plugin.recorder.pauseRecording();
+		this.plugin.timer.pause();
+	}
+
+	public async stopRecording() {
+		console.log("stopping recording...");
+		const blob = await this.plugin.recorder.stopRecording();
+		this.plugin.timer.reset();
+
+		const extension = this.plugin.recorder.getMimeType()?.split("/")[1];
+		const fileName = `${new Date()
+			.toISOString()
+			.replace(/[:.]/g, "-")}.${extension}`;
+		await this.plugin.audioHandler.fetchTranscription(blob, fileName);
+	}
+}
